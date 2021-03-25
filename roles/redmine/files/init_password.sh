bcd932dcdf92bd86d4e89acc960548d9621a563f
zengxc <zxc916684@163.com>
2021-03-25 14:01:56 +0800

    update init

--- roles/redmine/files/init_password.sh
+++ roles/redmine/files/init_password.sh
@@ -1,3 +1,3 @@
 new_password=$MYSQL_ROOT_PASSWORD
 app_pass=$(echo -n $new_password | sha1sum|awk 'BEGIN{ORS=""}{print $1}'|sha1sum|awk '{print $1}')
-mysql -uroot -p$new_password -e "use redmine;update users set hashed_password = '$app_pass', salt ='', admin = 1 where login = 'admin';"
+mysql -uroot -p$new_password -h 127.0.0.1 -e "use redmine;update users set hashed_password = '$app_pass', salt ='', admin = 1 where login = 'admin';"
